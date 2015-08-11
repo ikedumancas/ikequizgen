f6de17a71c38dbb1eba1a5030042ae472cb189df
Miguel Ike Dumancas Jr <ikedumancas.official@gmail.com>
2015-08-12 00:44:16 +0800

    updated wsgi.py

--- quizgen/wsgi.py
+++ quizgen/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for quizgen project.
+WSGI config for taskmgr project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -10,7 +10,8 @@ https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+from dj_static import Cling
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "quizgen.settings")
-
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "taskmgr.settings")
 application = get_wsgi_application()
+application = Cling(get_wsgi_application())
